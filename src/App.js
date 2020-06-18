d576c4f25849d757fa09dbef5291a9a8579e52f4
ryanp <ryanpanimation@gmail.com>
2020-06-18 00:47:24 -0500

    begin layout of card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import RecipeContext from "./contexts/RecipeContext";
+import Card from "./components/Card";
+import axiosWithAuth from "./utils/axiosWithAuth";
 
 function App() {
-  return <div className="App"></div>;
+  const [allRecipes, setAllRecipes] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("the recipes from backend")
+      .then((res) => {
+        console.log(res);
+        setAllRecipes(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <RecipeContext.Provider value={{}}>
+      <div className="App">
+        {allRecipes.map((item) => {
+          return <Card {...item} />;
+        })}
+      </div>
+    </RecipeContext.Provider>
+  );
 }
 
 export default App;
