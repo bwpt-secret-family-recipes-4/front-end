faff21f63b2c3077c943bea539d95a30586f7694
ryanp <ryanpanimation@gmail.com>
2020-06-19 01:54:40 -0500

    changes to display all fields on the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div>
+    <div className="card">
       <h3>{props.name}</h3>
       <p>{props.creator}</p>
+      <ul>
+        {props.ingredients.forEach((item) => {
+          return (
+            <>
+              <li>{item}</li>
+              <br />
+            </>
+          );
+        })}
+      </ul>
+      <p>{props.instructions}</p>
     </div>
   );
 };
