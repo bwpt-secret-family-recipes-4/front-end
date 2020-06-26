4f9c338d7d3c4c6e3cc6e68fabd7baa76927a866
Douglas Little <douglas.a.little@gmail.com>
2020-06-25 22:55:46 -0400

    Added validation to RecipeForm

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-// import * as yup from "yup";
+import * as yup from "yup";
 import axiosWithAuth from "../utils/axiosWithAuth";
 import RecipeContext from "../contexts/RecipeContext";
 
@@ -10,11 +10,20 @@ function RecipeForm() {
   const initialState = { title: "", creator: user, ingredients: "", directions: "", category: "" };
   const [recipe, setRecipe] = useState(initialState);
 
-  // const [errors, setErrors] = useState(initialState);
+  const [errors, setErrors] = useState(initialState);
 
-  // const formSchema = yup.object().shape({
+  const formSchema = yup.object().shape({
+    title: yup.string().required("Title is required."),
+    ingredients: yup.string().required("Ingredients are required."),
+    directions: yup.string().required("Directions are required."),
+    category: yup.string().required("Category is required")
+  });
 
-  // });
+  useEffect(() => {
+    formSchema.isValid(recipe).then((isFormValid) => {
+
+    }, [recipe, formSchema])
+  })
 
   const handleSubmit = (e, id) => {
     e.preventDefault();
@@ -29,14 +38,31 @@ function RecipeForm() {
       .catch((error) => console.log("Error", error));
   };
 
+  const validateChange = (e) => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((inputIsValid) => {
+        console.log("Input is validateChange.");
+        setErrors({ ...errors, [e.target.name]: ""});
+      })
+      .catch((error) => {
+        console.log("There is an error.");
+        setErrors({ ...errors, [e.target.name]: error.errors[0] });
+        console.log(errors);
+      });
+  };
+
   const handleChange = (e) => {
-    // console.log("Input Changed:", e.target.value);
-    // console.log("Input that fired event:", e.target.name);
+    e.persist();
+    console.log("Input Changed:", e.target.value);
+    console.log("Input that fired event:", e.target.name);
 
     const newRecipe = {
       ...recipe,
       [e.target.name]: e.target.value,
     };
+    validateChange(e);
     setRecipe(newRecipe);
   };
 
@@ -45,25 +71,23 @@ function RecipeForm() {
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">
           Title:
-          <input id="title" type="text" name="title" onChange={handleChange} value={recipe.title} />
-        </label>
-        <label htmlFor="creator">
-          Creator:
-          <input
-            id="creator"
-            type="text"
-            name="creator"
-            onChange={handleChange}
-            value={recipe.user}
+          <input id="title"
+          type="text"
+          name="title"
+          onChange={handleChange}
+          value={recipe.title}
           />
+          {errors.title.length > 0 ? <span className="error">{errors.title}</span> : null}
         </label>
         <label htmlFor="ingredients">
           Ingedients
-          <textarea name="ingredients" rows="5" cols="50" maxlength="10000"></textarea>
+          <textarea name="ingredients" rows="5" cols="50" maxLength="10000" onChange={handleChange} value= {recipe.ingredients}></textarea>
+          {errors.ingredients.length > 0 ? <span className="error">{errors.ingredients}</span> : null}
         </label>
         <label htmlFor="directions">
           Directions
-          <textarea name="directions" rows="5" cols="50" maxlength="10000"></textarea>
+          <textarea name="directions" rows="5" cols="50" maxLength="10000" onChange={handleChange} value={recipe.directions}></textarea>
+          {errors.directions.length > 0 ? <span className="error">{errors.directions}</span> : null}
         </label>
         <label htmlFor="category">
           Category - Breakfast, Lunch, Dinner, Dessert, etc.
@@ -74,6 +98,7 @@ function RecipeForm() {
             onChange={handleChange}
             value={recipe.category}
           />
+          {errors.category.length > 0 ? <span className="error">{errors.category}</span> : null}
         </label>
         <br />
         <br />
